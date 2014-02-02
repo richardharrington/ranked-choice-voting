08fee835c762e7e7d39045b526bde55ea6bec4e2
Richard Harrington <harrington.richard@gmail.com>
2014-02-02 17:23:28 -0500

    added most preliminary functions

--- src/ranked_choice/core.clj
+++ src/ranked_choice/core.clj
@@ -1,96 +1,57 @@
 (ns ranked-choice.core)
 
+(def majority-win-data
+  [{"Bob" 1, "Sally" 2, "Maurice" 3}
+   {"Bob" 1, "Sally" 3, "Maurice" 2}
+   {"Bob" 2, "Sally" 1, "Maurice" 3}])
+
 (def data
-  [{"Sally" 3
-    "Bob" 2
-    "Maurice" 1
-    "Susan" 4}
-   {"Sally" 3
-    "Bob" 2
-    "Maurice" 1
-    "Susan" 4}
-   {"Sally" 3
-    "Bob" 2
-    "Maurice" 1
-    "Susan" 4}
-   {"Sally" 3
-    "Bob" 2
-    "Maurice" 1
-    "Susan" 4}
-   {"Sally" 3
-    "Bob" 2
-    "Maurice" 1
-    "Susan" 4}
-
-   {"Sally" 3
-    "Bob" 2
-    "Maurice" 4
-    "Susan" 1}
-   {"Sally" 3
-    "Bob" 2
-    "Maurice" 4
-    "Susan" 1}
-   {"Sally" 3
-    "Bob" 2
-    "Maurice" 4
-    "Susan" 1}
-   {"Sally" 3
-    "Bob" 2
-    "Maurice" 4
-    "Susan" 1}
-
-   {"Sally" 2
-    "Bob" 1
-    "Maurice" 4
-    "Susan" 3}
-   {"Sally" 2
-    "Bob" 1
-    "Maurice" 4
-    "Susan" 3}
-   {"Sally" 2
-    "Bob" 1
-    "Maurice" 4
-    "Susan" 3}
-   {"Sally" 1
-    "Bob" 2
-    "Maurice" 4
-    "Susan" 3}
-   {"Sally" 1
-    "Bob" 2
-    "Maurice" 4
-    "Susan" 3}])
+  [{"Sally" 3, "Bob" 2, "Maurice" 1, "Susan" 4}
+   {"Sally" 3, "Bob" 2, "Maurice" 1, "Susan" 4}
+   {"Sally" 3, "Bob" 2, "Maurice" 1, "Susan" 4}
+   {"Sally" 3, "Bob" 2, "Maurice" 1, "Susan" 4}
+   {"Sally" 3, "Bob" 2, "Maurice" 1, "Susan" 4}
+   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
+   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
+   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
+   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
+   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
+   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
+   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
+   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}
+   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}])
 
 
 ; initial ballots consist of a sequence
 ; of vectors of maps of names (strings) to preference numbers (integers).
 
+(defn majority-score [items]
+  (/ (count items) 2))
 
-(defn get-ballot-vectors [ballots]
+(defn ballot-vectors [ballots]
   (map (fn [ballot]
          (map key (sort-by val ballot)))
        ballots))
 
-(defn vote [ballots]
-  (let [total-to-beat (/ (count ballots) 2)
-        find-winner (fn [candidate-totals]
-                 (if-let [winner-map-entry (some #(> (val %) total-to-beat)
-                                                 candidate-totals)]
-                   (key winner-map-entry)
-                   nil))
-        find-loser (fn [candidate-totals]
-                (key (reduce (fn [acc candidate-total]
-                               (if (< (val candidate-total) (val acc))
-                                 candidate-total
-                                 acc))
-                             candidate-totals)))]
-    (loop [ballot-vectors (map (fn [ballot]
-                                  (map key (sort-by val ballot)))
-                                ballots)]
-      (let [candidate-totals (frequencies (map first ballot-vectors))]
-        (or (find-winner candidate-totals)
-            (let [loser (find-loser candidate-totals)]
-              (recur (map (fn [ballot-vector]
-                            (if (= (first ballot-vector) loser)
-                              (rest ballot-vector)
-                              ballot-vector))
-                          ballot-vectors))))))))
+(defn candidate-frequencies [ballots]
+  (frequencies (map first (ballot-vectors ballots))))
+
+(defn high-or-low-scorer [max-or-min ballots]
+  "if there's a tie, pick a random one from among the ties"
+  (let [candidate-freqs (candidate-frequencies ballots)
+        high-or-low-score (apply max-or-min (vals candidate-freqs))
+        high-or-low-scorers (keys (filter #(= (val %) high-or-low-score)
+                                     candidate-freqs))]
+    (rand-nth (seq high-or-low-scorers))))
+
+(defn high-scorer [ballots]
+  (high-or-low-scorer max ballots))
+
+(defn low-scorer [ballots]
+  (high-or-low-scorer min ballots))
+
+(defn majority-winner [ballots]
+  (let [candidate-freqs (candidate-frequencies ballots)
+        [winner high-score] (apply max-key val candidate-freqs)]
+    (when (> high-score (majority-score ballots))
+      winner)))
