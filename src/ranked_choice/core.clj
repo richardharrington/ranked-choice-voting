e8d58a5eb0f53dcba4c272513d5e0c533fbfae78
Richard Harrington <harrington.richard@gmail.com>
2014-02-02 18:58:25 -0500

    working now

--- src/ranked_choice/core.clj
+++ src/ranked_choice/core.clj
@@ -1,9 +1,14 @@
-(ns ranked-choice.core)
+(ns ranked-choice.core
+  (:use [clojure.pprint :only [pprint]]))
 
 (def majority-win-data
   [{"Bob" 1, "Sally" 2, "Maurice" 3}
    {"Bob" 1, "Sally" 3, "Maurice" 2}
    {"Bob" 2, "Sally" 1, "Maurice" 3}])
+
+(def tie-data
+  [{"Bob" 1}
+   {"Sally" 1}])
 
 (def data
   [{"Sally" 3, "Bob" 2, "Maurice" 1, "Susan" 4}
@@ -25,33 +30,47 @@
 ; initial ballots consist of a sequence
 ; of vectors of maps of names (strings) to preference numbers (integers).
 
-(defn majority-score [items]
-  (/ (count items) 2))
-
-(defn ballot-vectors [ballots]
+(defn candidate-sequences [ballots]
   (map (fn [ballot]
          (map key (sort-by val ballot)))
        ballots))
 
-(defn candidate-frequencies [ballots]
-  (frequencies (map first (ballot-vectors ballots))))
+(defn candidate-frequencies [candidate-seqs]
+  (frequencies (map first candidate-seqs)))
 
-(defn high-or-low-scorer [max-or-min ballots]
+(defn high-or-low-scorer [max-or-min candidate-seqs]
   "if there's a tie, pick a random one from among the ties"
-  (let [candidate-freqs (candidate-frequencies ballots)
+  (let [candidate-freqs (candidate-frequencies candidate-seqs)
         high-or-low-score (apply max-or-min (vals candidate-freqs))
         high-or-low-scorers (keys (filter #(= (val %) high-or-low-score)
-                                     candidate-freqs))]
-    (rand-nth (seq high-or-low-scorers))))
+                                          candidate-freqs))]
+    (rand-nth high-or-low-scorers)))
 
-(defn high-scorer [ballots]
-  (high-or-low-scorer max ballots))
+(def high-scorer (partial high-or-low-scorer max))
 
-(defn low-scorer [ballots]
-  (high-or-low-scorer min ballots))
+(def low-scorer (partial high-or-low-scorer min))
 
-(defn majority-winner [ballots]
-  (let [candidate-freqs (candidate-frequencies ballots)
+(defn majority-winner [candidate-seqs]
+  (let [majority-threshold (/ (count candidate-seqs) 2)
+        candidate-freqs (candidate-frequencies candidate-seqs)
         [winner high-score] (apply max-key val candidate-freqs)]
-    (when (> high-score (majority-score ballots))
+    (when (> high-score majority-threshold)
       winner)))
+
+(defn winning-candidate [candidate-seqs]
+  (or (majority-winner candidate-seqs)
+      (and (= (count (first candidate-seqs)) 1)
+           (high-scorer candidate-seqs))))
+
+(defn next-round [candidate-seqs]
+  (let [loser (low-scorer candidate-seqs)]
+    (map #(remove #{loser} %) candidate-seqs)))
+
+(defn vote [ballots]
+  (loop [candidate-seqs (candidate-sequences ballots)]
+    (pprint (sort-by first candidate-seqs))
+    (if-let [winner (winning-candidate candidate-seqs)]
+      winner
+      (recur (next-round candidate-seqs)))))
+
+
