325b6db6d27925db4a7833991928c22d365ecfa9
Richard Harrington <harrington.richard@gmail.com>
2014-02-02 22:07:46 -0500

    actually added the test data this time

---
+++ src/ranked_choice/test_data.clj
@@ -0,0 +1,29 @@
+(ns ranked-choice.test-data)
+
+
+(def majority-in-first-round
+  [{"Bob" 1, "Sally" 2, "Maurice" 3}
+   {"Bob" 1, "Sally" 3, "Maurice" 2}
+   {"Bob" 2, "Sally" 1, "Maurice" 3}])
+
+(def tie
+  [{"Bob" 1}
+   {"Sally" 1}])
+
+(def majority-in-third-round
+  [{"Sally" 3, "Bob" 2, "Maurice" 1, "Susan" 4}
+   {"Sally" 3, "Bob" 2, "Maurice" 1, "Susan" 4}
+   {"Sally" 3, "Bob" 2, "Maurice" 1, "Susan" 4}
+   {"Sally" 3, "Bob" 2, "Maurice" 1, "Susan" 4}
+   {"Sally" 3, "Bob" 2, "Maurice" 1, "Susan" 4}
+   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
+   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
+   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
+   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
+   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
+   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
+   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
+   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}
+   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}])
+
+
