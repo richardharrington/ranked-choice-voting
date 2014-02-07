5670a9d3c9395b3a45bce1da82b93307071385f3
Richard Harrington <rharrington@amplify.com>
2014-02-06 22:57:11 -0500

    ditched commented-out garbage

--- src/cljs/ranked_choice/core.cljs
+++ src/cljs/ranked_choice/core.cljs
@@ -45,17 +45,6 @@
 (defn next-rank []
   (first-nonconsecutive (remove nil? (map second (deref-choices)))))
 
-; (defn lowest-rank []
-;   (apply min-key second (deref-choices)))
-
-; (defn update-winner! []
-;   (let [rank (lowest-rank)
-;         choices (deref-choices)]
-;     (log rank)
-;     (log choices)
-;     (swap! app-state assoc :winner (first (nth choices rank)))))
-
-
 (defn update-choices! [f & args]
   (apply swap! app-state update-in [:choices] f args))
 
@@ -80,9 +69,7 @@
   (let [choices (deref-choices)]
     (if (second (nth choices (index-of-name name)))
       (remove-rank! name)
-      (add-rank! name (next-rank)))
-    ; (update-winner!)
-    ))
+      (add-rank! name (next-rank)))))
 
 (defn choice-list-component []
   [:div
@@ -104,93 +91,3 @@
   (reagent/render-component [component] element))
 
 (mount main-page (by-id "ranked-choice-root"))
-
-
-
-
-
-
-
-; (def app-state
-;   (atom
-;     {:contacts
-;      [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
-;       {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
-;       {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
-;       {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
-;       {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
-;       {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
-
-; (defn update-contacts! [f & args]
-;   (apply swap! app-state update-in [:contacts] f args))
-
-; (defn add-contact! [c]
-;   (update-contacts! conj c))
-
-; (defn remove-contact! [c]
-;   (update-contacts! (fn [contacts c]
-;                       (vec (remove #{c} contacts)))
-;                     c))
-
-; (defn parse-contact [contact-str]
-;   (let [[first middle last :as parts] (string/split contact-str #"\s+")
-;         [first last middle] (if (nil? last) [first middle] [first last middle])
-;         middle (when middle (string/replace middle "." ""))
-;         c (if middle (count middle) 0)]
-;     (when (>= (reduce + (map #(if % 1 0) parts)) 2)
-;       (cond-> {:first first :last last}
-;         (= c 1)  (assoc :middle-initial middle)
-;         (> c 1) (assoc :middle middle)))))
-
-; (defn middle-name [{:keys [middle middle-initial]}]
-;   (cond
-;     middle (str " " middle)
-;     middle-initial (str " " middle-initial ".")))
-
-; (defn display-name [{:keys [first last] :as contact}]
-;   (str last ", " first (middle-name contact)))
-
-
-
-; (defn contact [c]
-;   [:li
-;    [:span (display-name c)]
-;    [:button {:on-click #(remove-contact! c)}
-;     "Delete"]])
-
-; (def new-contact
-;   (let [valu (reagent/atom "")]
-;     (fn []
-;       [:div
-;        [:input {:type "text"
-;                 :placeholder "Contact Name"
-;                 :value @valu
-;                 :on-change #(reset! valu (-> % .-target .-value))}]
-;        [:button {:on-click #(when-let [c (parse-contact @valu)]
-;                               (add-contact! c)
-;                               (reset! val ""))}
-;         "Add"]])))
-
-; (defn contact-list []
-;   [:div
-;    [:h1 "Contact list"]
-;    [:ul
-;     (for [c (:contacts @app-state)]
-;       [contact c])]
-;    [new-contact]])
-
-
-
-; ;------------------------------------------------------
-
-; (defn main-page []
-;   [:div
-;    [:h1 "Reagent playground"]
-;    [contact-list]])
-
-; (defn mount [component element]
-;   (reagent/render-component [component] element))
-
-; (mount main-page (by-id "reagent-tut"))
-
-
