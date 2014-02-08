e3ce8f92972e91cbdff639bd118e8fef98d05d53
Richard Harrington <rharrington@amplify.com>
2014-02-07 23:20:28 -0500

    changed test data from names to rainbows and puppies and stuff

--- src/clj/ranked_choice/test_data.clj
+++ src/clj/ranked_choice/test_data.clj
@@ -1,63 +1,63 @@
 (ns ranked-choice.test-data)
 
 (def simple-tie
-  [{"Bob" 1, "Sally" 2}
-   {"Bob" 2, "Sally" 1}])
+  [{"Bob" 1, "rainbows" 2}
+   {"Bob" 2, "rainbows" 1}])
 
 (def first-round-majority
-  [{"Bob" 1, "Sally" 2, "Maurice" 3}
-   {"Bob" 1, "Sally" 3, "Maurice" 2}
-   {"Bob" 2, "Sally" 1, "Maurice" 3}])
+  [{"Bob" 1, "rainbows" 2, "video games" 3}
+   {"Bob" 1, "rainbows" 3, "video games" 2}
+   {"Bob" 2, "rainbows" 1, "video games" 3}])
 
 (def regular-ballots-no-tie
-  [{"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
-   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
-   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
-   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
-   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
-   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
-   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
-   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}
-   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}])
+  [{"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 3, "puppies" 2, "video games" 4, "ice cream" 1}
+   {"rainbows" 3, "puppies" 2, "video games" 4, "ice cream" 1}
+   {"rainbows" 3, "puppies" 2, "video games" 4, "ice cream" 1}
+   {"rainbows" 3, "puppies" 2, "video games" 4, "ice cream" 1}
+   {"rainbows" 2, "puppies" 1, "video games" 4, "ice cream" 3}
+   {"rainbows" 2, "puppies" 1, "video games" 4, "ice cream" 3}
+   {"rainbows" 2, "puppies" 1, "video games" 4, "ice cream" 3}
+   {"rainbows" 1, "puppies" 2, "video games" 4, "ice cream" 3}
+   {"rainbows" 1, "puppies" 2, "video games" 4, "ice cream" 3}])
 
 (def regular-ballots-tie
-  [{"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
-   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
-   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
-   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
-   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
-   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
-   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
-   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}
-   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}
-   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}])
+  [{"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 3, "puppies" 2, "video games" 4, "ice cream" 1}
+   {"rainbows" 3, "puppies" 2, "video games" 4, "ice cream" 1}
+   {"rainbows" 3, "puppies" 2, "video games" 4, "ice cream" 1}
+   {"rainbows" 3, "puppies" 2, "video games" 4, "ice cream" 1}
+   {"rainbows" 2, "puppies" 1, "video games" 4, "ice cream" 3}
+   {"rainbows" 2, "puppies" 1, "video games" 4, "ice cream" 3}
+   {"rainbows" 2, "puppies" 1, "video games" 4, "ice cream" 3}
+   {"rainbows" 1, "puppies" 2, "video games" 4, "ice cream" 3}
+   {"rainbows" 1, "puppies" 2, "video games" 4, "ice cream" 3}
+   {"rainbows" 1, "puppies" 2, "video games" 4, "ice cream" 3}])
 
 (def irregular-ballots
   [{"Twinkies" 1, "Jalapeños" 2}
-   {"Sally" 5, "Bob" 4, "Maurice" 2, "Susan" 3, "Garbonzo Beans" 1}
-   {"Sally" 5, "Bob" 4, "Maurice" 2, "Susan" 3, "Garbonzo Beans" 1}
-   {"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 4, "Bob" 3, "Maurice" 1, "Susan" 2}
-   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
-   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
-   {"Sally" 3, "Bob" 2, "Maurice" 4, "Susan" 1}
-   {"Sally" 4, "Bob" 3, "Maurice" 5, "Susan" 2, "Chilis" 1}
-   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
-   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
-   {"Sally" 2, "Bob" 1, "Maurice" 4, "Susan" 3}
-   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}
-   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}
-   {"Sally" 1, "Bob" 2, "Maurice" 4, "Susan" 3}])
+   {"rainbows" 5, "puppies" 4, "video games" 2, "ice cream" 3, "Garbonzo Beans" 1}
+   {"rainbows" 5, "puppies" 4, "video games" 2, "ice cream" 3, "Garbonzo Beans" 1}
+   {"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 4, "puppies" 3, "video games" 1, "ice cream" 2}
+   {"rainbows" 3, "puppies" 2, "video games" 4, "ice cream" 1}
+   {"rainbows" 3, "puppies" 2, "video games" 4, "ice cream" 1}
+   {"rainbows" 3, "puppies" 2, "video games" 4, "ice cream" 1}
+   {"rainbows" 4, "puppies" 3, "video games" 5, "ice cream" 2, "Chilis" 1}
+   {"rainbows" 2, "Bob" 1, "video games" 4, "ice cream" 3}
+   {"rainbows" 2, "Bob" 1, "video games" 4, "ice cream" 3}
+   {"rainbows" 2, "Bob" 1, "video games" 4, "ice cream" 3}
+   {"rainbows" 1, "Bob" 2, "video games" 4, "ice cream" 3}
+   {"rainbows" 1, "Bob" 2, "video games" 4, "ice cream" 3}
+   {"rainbows" 1, "Bob" 2, "video games" 4, "ice cream" 3}])
 
 
